128b5c45fdb49c9789daef0c97013e8220d76eea
Swapnil Thawale <92202649+Swapnil213@users.noreply.github.com>
2025-04-08 00:00:39 +0530

    Add files via upload

--- Solutions.java
+++ Solutions.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solutions {
@@ -19,8 +20,31 @@ public class Solutions {
         return true;
     }
 
+    static boolean isAnagram(String a, String b) {
+        char ar[] = a.toCharArray();
+        char br[] = b.toCharArray();
+
+        int len1 = ar.length;
+        int len2 = br.length;
+
+        if (len1 != len2) {
+            return false;
+        }
+
+        Arrays.sort(ar);
+        Arrays.sort(br);
+
+        for (int i = 0; i < br.length; i++) {
+            if (ar[i] != br[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String to check palindrome");
         String name = sc.nextLine();
         if (isPalindrome(name)) {
             System.out.println(name + " is a palindrome");
@@ -28,6 +52,18 @@ public class Solutions {
         else {
             System.out.println(name + " is not a palindrome");
         }
+
+        System.out.println("Enter Strings to check Anagram");
+
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        if (isAnagram(s1, s2)) {
+            System.out.println(s1 + " & " + s2 + " are Anagram");
+        }
+        else {
+            System.out.println(s1 + " & " + s2 + " are not Anagram");
+        }
+
         sc.close();
     }
 }
