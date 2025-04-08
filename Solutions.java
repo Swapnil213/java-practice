cb3f30c04f4bb967d80541709ed1ddd2601266ed
Swapnil Thawale <92202649+Swapnil213@users.noreply.github.com>
2025-04-08 23:26:39 +0530

    Add files via upload

--- Solutions.java
+++ Solutions.java
@@ -42,14 +42,46 @@ public class Solutions {
         return true;
     }
 
+    public static void printFibonacciSequence(int count) {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+
+        for (int i = 1; i <= count; i++) {
+            System.out.print(a + " ");
+            a = b;
+            b = c;
+            c = a + b;
+        }
+    }
+
+    public static int fibonacci(int count) {
+        if (count <= 1)
+            return count;
+
+        return fibonacci(count - 1) + fibonacci(count - 2);
+    }
+
+    static String removeWhiteSpaces(String input) {
+        StringBuilder output = new StringBuilder();
+
+        char[] charArray = input.toCharArray();
+
+        for (char c : charArray) {
+            if (!Character.isWhitespace(c))
+                output.append(c);
+        }
+
+        return output.toString();
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter String to check palindrome");
         String name = sc.nextLine();
         if (isPalindrome(name)) {
             System.out.println(name + " is a palindrome");
-        }
-        else {
+        } else {
             System.out.println(name + " is not a palindrome");
         }
 
@@ -59,10 +91,25 @@ public class Solutions {
         String s2 = sc.nextLine();
         if (isAnagram(s1, s2)) {
             System.out.println(s1 + " & " + s2 + " are Anagram");
-        }
-        else {
+        } else {
             System.out.println(s1 + " & " + s2 + " are not Anagram");
         }
+
+        printFibonacciSequence(10);
+
+        System.out.println();
+
+        int length = 10;
+
+        System.out.print("A Fibonacci sequence of " + length + " numbers: ");
+
+        for (int i = 0; i < length; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+
+        System.out.println();
+
+        System.out.println(removeWhiteSpaces("I am a Student"));
 
         sc.close();
     }
