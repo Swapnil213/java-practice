d0c2967269f313f1f2297185f1e86ff44c351077
Swapnil Thawale <92202649+Swapnil213@users.noreply.github.com>
2025-04-10 23:01:52 +0530

    Add files via upload

--- IBMpractice.java
+++ IBMpractice.java
@@ -57,6 +57,48 @@ public class IBMpractice {
         }
         return binary;
     }
+
+    static String GetReverse(String val) {
+        char arr[] = val.toCharArray();
+        String result = "";
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            result += arr[i];
+        }
+        return result;
+    }
+    
+    static Boolean IsPrime(int num) {
+        int count = 0;
+
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                count++;
+                break;
+            }
+        }
+
+        if (count == 0) {
+            return true;
+        }
+        return false;
+    }
+    
+    static int CountVowels(String val) {
+        int count = 0;
+        char arr[] = val.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            char ch = arr[i];
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'
+            || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         // System.out.println("Enter numbers to find HCF : ");
@@ -75,11 +117,31 @@ public class IBMpractice {
 
         // System.out.println("Sum of Digits in " + a + " is : "+ FindSumOfDigits(a));
 
-        System.out.println("Enter number to find its Binary : ");
-        int a = sc.nextInt();
+        // System.out.println("Enter number to find its Binary : ");
+        // int a = sc.nextInt();
 
-        System.out.println("Factorial of "+ a + " is : "+ GetBinary(a));
+        // System.out.println("Factorial of "+ a + " is : "+ GetBinary(a));
 
+        // System.out.println("Enter String to Reverse : ");
+        // String val = sc.nextLine();
+
+        // System.out.println("Reverse of "+ val + " is : "+ GetReverse(val));
+
+        // System.out.println("Enter number to find if its Prime : ");
+        // int a = sc.nextInt();
+
+        // if (IsPrime(a)) {
+        //     System.out.println(a + " is Prime");
+        // }
+        // else {
+        //     System.out.println(a + " is not Prime");
+        // }
+
+        System.out.println("Enter String to find number of vowels in it : ");
+        String val = sc.nextLine();
+
+        System.out.println("Number of vowels in "+ val + " is : "+ CountVowels(val));
+        
         sc.close();
     }
 }
