62a2dd267372d26cb0a5824775544adc717630a3
Swapnil Thawale <92202649+Swapnil213@users.noreply.github.com>
2025-04-15 23:17:01 +0530

    Add files via upload

---
+++ FindNthFabonacci.java
@@ -0,0 +1,13 @@
+public class FindNthFabonacci {
+        public static int F(int n) {
+            if (n <= 1) {
+                return n;
+            } else {
+                return F(n - 1) + F(n - 2);
+            }
+        }
+
+        public static void main(String[] args) {
+            System.out.println(F(10));
+        }
+}
