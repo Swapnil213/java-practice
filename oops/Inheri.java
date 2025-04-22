39143cf1834faa72175e1ead8fc98c8c931e0b10
Swapnil Thawale <92202649+Swapnil213@users.noreply.github.com>
2025-04-22 23:31:26 +0530

    Add files via upload

---
+++ oops/Inheri.java
@@ -0,0 +1,34 @@
+package oops;
+
+// Superclass (Parent)
+class Animal {
+    void eat() {
+        System.out.println("Animal is eating...");
+    }
+
+    void sleep() {
+        System.out.println("Animal is sleeping...");
+    }
+}
+
+// Subclass (Child) - Inherits from Animal
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog is barking!");
+    }
+}
+
+
+
+public class Inheri {
+        public static void main(String[] args) {
+            Dog myDog = new Dog();
+
+            // Inherited methods (from Animal)
+            myDog.eat();
+            myDog.sleep();
+
+            // Child class method
+            myDog.bark();
+        }
+}
